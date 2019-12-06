6404f8e1e2bd8b6b596ec5d34c5e71cf39da04e8
zhangxiaojun <765845148@qq.com>
2019-12-06 22:01:52 +0800

    更

--- src/pages/all/index.js
+++ src/pages/all/index.js
@@ -1,8 +1,11 @@
 import React,{Component} from "react";
 export default class All extends Component{
-    render(){
-        return(
-            <div>111</div>
+    render() {
+        return (
+            <div>111
+                <div>99</div>
+            </div>
+            
         )
     }
 }
